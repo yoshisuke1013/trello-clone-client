6c02d19a1fd2e7214c680ff418581551099e0eb3
yoshisuke1013 <shin10132022@gmail.com>
2025-09-11 09:15:56 +0900

    ログイン処理

--- src/modules/auth/auth.repository.ts
+++ src/modules/auth/auth.repository.ts
@@ -15,4 +15,16 @@ export const authRepository = {
     const { user, token } = result.data;
     return { user: new User(user), token };
   },
+
+  async signin(
+    email: string,
+    password: string
+  ): Promise<{ user: User; token: string }> {
+    const result = await api.post("/auth/signin", {
+      email,
+      password,
+    });
+    const { user, token } = result.data;
+    return { user: new User(user), token };
+  },
 };
